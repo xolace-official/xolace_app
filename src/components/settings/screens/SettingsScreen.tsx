ae91808bc5388b2fe0f7f925595d4d87639a1c19
Nathaniel E. Adama <kvngnathan8420@gmail.com>
2026-03-11 11:20:23 +0000

    Add settings ui

---
+++ src/components/settings/screens/SettingsScreen.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export const SettingsScreen = () => {
+  return (
+    <View>
+      <Text>SettingsScreen</Text>
+    </View>
+  )
+}
