c7028995ea143c39de47f4ac42dc34b1bb717477
Nathaniel E. Adama <kvngnathan8420@gmail.com>
2026-03-13 07:18:15 +0000

    Add skills & some deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,12 @@
 /* eslint-env node */
+import convexPlugin from "@convex-dev/eslint-plugin";
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
 
+
 module.exports = defineConfig([
   expoConfig,
+  ...convexPlugin.configs.recommended,
   {
     ignores: ['dist/*'],
   },
