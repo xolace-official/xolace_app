09252f038c9e70cfb952c15379469f759c72208e
Nathaniel E. Adama <kvngnathan8420@gmail.com>
2026-08-20 16:38:02 +0000

    Update api.d.ts

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -80,6 +80,7 @@ import type * as lib_aggregates from "../lib/aggregates.js";
 import type * as lib_auth from "../lib/auth.js";
 import type * as lib_camperTag from "../lib/camperTag.js";
 import type * as lib_chatNotifications from "../lib/chatNotifications.js";
+import type * as lib_claimStrength from "../lib/claimStrength.js";
 import type * as lib_conversationGating from "../lib/conversationGating.js";
 import type * as lib_displayName from "../lib/displayName.js";
 import type * as lib_followUpCadence from "../lib/followUpCadence.js";
@@ -203,6 +204,7 @@ declare const fullApi: ApiFromModules<{
   "lib/auth": typeof lib_auth;
   "lib/camperTag": typeof lib_camperTag;
   "lib/chatNotifications": typeof lib_chatNotifications;
+  "lib/claimStrength": typeof lib_claimStrength;
   "lib/conversationGating": typeof lib_conversationGating;
   "lib/displayName": typeof lib_displayName;
   "lib/followUpCadence": typeof lib_followUpCadence;
