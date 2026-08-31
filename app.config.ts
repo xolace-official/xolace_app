4dad4905079ca7cfe800552ceeeaa7eaeb4375b5
Nathaniel E. Adama <kvngnathan8420@gmail.com>
2026-08-31 22:13:30 +0000

    version bump

--- app.config.ts
+++ app.config.ts
@@ -56,7 +56,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     ...config,
     name: getAppName(),
     slug: "xolace",
-    version: "1.9.1",
+    version: "1.10.0",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "xolace",
